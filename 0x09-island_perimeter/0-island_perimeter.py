0f5d5745d0a7c4780798591745ce63a001a1acc3
kadimakeith <keithkadima@gmail.com>
2023-08-31 18:23:43 +0300

    update island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,15 +1,25 @@
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
+    """Returns the perimeter of the island described in grid
+        grid is a list of list of integers:
+        0 represents water
+        1 represents land
+        Each cell is square, with a side length of 1
+        Cells are connected horizontally/vertically (not diagonally).
+        grid is rectangular, with its width and height not exceeding 100
+        The grid is completely surrounded by water
+        There is only one island (or nothing).
+        The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island)."""
+
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                if i == 0 or not grid[i - 1][j]:
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row - 1 < 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-                if j == 0 or not grid[i][j - 1]:
+                if row + 1 >= len(grid) or grid[row + 1][col] == 0:
                     perimeter += 1
-                if i == len(grid) - 1 or not grid[i + 1][j]:
+                if col - 1 < 0 or grid[row][col - 1] == 0:
                     perimeter += 1
-                if j == len(grid[i]) - 1 or not grid[i][j + 1]:
+                if col + 1 >= len(grid[row]) or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
